2a091fb28c68e2c9f8e91ad16f78c74a66cd220d
suuuuuuu <70851874+201910086@users.noreply.github.com>
2023-01-01 21:08:34 +0900

    [Bronze II] Title: 분해합, Time: 224 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+        for (int i=1; i<N; i++) {
+            int num = i;
+            int sum = 0;
+            while (num != 0) {
+                sum += num%10;
+                num /= 10;
+            }
+            if (sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
