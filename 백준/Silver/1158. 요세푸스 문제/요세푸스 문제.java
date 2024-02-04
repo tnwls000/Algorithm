fedb125c83a72df7be703b2b9079d16459e00ac0
sujin <70851874+tnwls000@users.noreply.github.com>
2024-02-04 19:46:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 548 ms, Memory: 295532 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,27 +1,38 @@
-import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); //사람 수
-		int k = sc.nextInt(); //제거될 순번
-		
-		//1번부터 N번까지의 사람 큐에 넣기
-		Queue<Integer> q = new LinkedList<>();
-		for (int i=1; i<=n; i++) {
-			q.offer(i);
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); // n : 사람 수
+		int k = Integer.parseInt(st.nextToken()); // k : 제거할 사람의 인덱스
+	
+		Deque<Integer> list = new ArrayDeque<>();
+		for (int i=1; i<=n; i++) { // 1번째부터 n번째까지의 사람 추가
+			list.add(i);
 		}
 		
-		String result = "<";
-		while (q.size() > 1) {
-			for (int i=0; i<k-1; i++) {
-				q.offer(q.poll());
+		int idx = 0;
+		String[] josephus = new String[n];
+		while (!list.isEmpty()) { // 사람 다 제거될 때까지
+			for (int i=0; i<k-1; i++) { // k-1번째까지는 앞에꺼 뒤로 보냄
+				int tmp = list.poll(); 
+				list.addLast(tmp);
 			}
-			result += q.poll() + ", ";
+			josephus[idx++] = String.valueOf(list.poll()); // k번째 사람
 		}
-		result += q.poll() + ">";
-		System.out.println(result);
-    }
+		
+		String result = String.join(", ", josephus);
+		bw.write("<" + result + ">");
+		bw.flush();
+		bw.close();
+	}
 }
