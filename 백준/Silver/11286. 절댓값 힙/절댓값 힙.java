9e4dc9ee0dc24c668b20d83a7d4c9053a1127678
sujin <70851874+tnwls000@users.noreply.github.com>
2024-02-13 23:16:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 336 ms, Memory: 25592 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			if (Math.abs(o1) == Math.abs(o2)) {
+				return o1 - o2;
+			}
+			return Math.abs(o1) - Math.abs(o2);
+		});
+		
+		for (int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				if (pq.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.add(num);
+			}
+		}
+		System.out.println(sb);
+	}
+}
