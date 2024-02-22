0df7f2e1cb758aad7a848f3044182a908db90758
sujin <70851874+tnwls000@users.noreply.github.com>
2024-02-22 23:46:09 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,59 +1,63 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
-	static boolean visit[][];
-	static int start, end, n, m, map[][];
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	
-    public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		map = new int[n][m];
-		visit = new boolean[n][m];
-		for (int x=0; x<n; x++) {
-			String str = br.readLine();
-			for (int y=0; y<m; y++) {
-				map[x][y] = Integer.parseInt(str.substring(y,y+1));
-			}
-		}
-		
-		visit[0][0] = true;
-		//하,우로 이동하는데 값이 0이면 이동불가
-		//end에 도달하면 칸 수 return
-		//end까지 도달하는데 최소 칸 수
-		bfs(0, 0);
-		System.out.println(map[n-1][m-1]);
+class Point {
+	int x;
+	int y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
-	
-	private static void bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {x,y});
-		
-		while (!queue.isEmpty()) {
-			int[] point = queue.poll();
-			int nowX = point[0];
-			int nowY = point[1];
-			for (int i=0; i<4; i++) {
-				int cx = nowX + dx[i];
-				int cy = nowY + dy[i];
-				if (cx >=0 && cy >= 0 && cx < n && cy < m){
-					if (map[cx][cy] == 1 && !visit[cx][cy]) {
-						map[cx][cy] = map[nowX][nowY] + 1;
-						visit[cx][cy] = true;
-						queue.offer(new int[] {cx, cy});
-					}
-				}
+}
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] maze = new int[n][m];
+		int[][] visited = new int[n][m];
+		for (int[] v : visited) {
+			Arrays.fill(v, -1); // 방문 배열로 이동거리 셀 것임
+		}
+
+		for (int r = 0; r < n; r++) {
+			String str = br.readLine();
+			for (int c = 0; c < m; c++) {
+				maze[r][c] = str.charAt(c) - '0';
 			}
 		}
+
+		int[] dr = { -1, 1, 0, 0 };
+		int[] dc = { 0, 0, -1, 1 };
+
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(0, 0)); // 시작위치
+		visited[0][0] = 1;
+
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nowR = p.x + dr[i];
+				int nowC = p.y + dc[i];
+
+				if (nowR < 0 || nowR >= n || nowC < 0 || nowC >= m)
+					continue;
+				if (visited[nowR][nowC] != -1 || maze[nowR][nowC] == 0)
+					continue;
+				visited[nowR][nowC] = visited[p.x][p.y] + 1;
+				q.add(new Point(nowR, nowC));
+			}
+		}
+
+		System.out.println(visited[n - 1][m - 1]);
 	}
 }
