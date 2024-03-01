ca00dbaad8ef3c3185e23f86ca55aea9a6a0bbba
sujin <70851874+tnwls000@users.noreply.github.com>
2024-03-01 13:48:06 +0900

    [Silver III] Title: N과 M (6), Time: 144 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int n, m;
+	static int[] result, arr;
+	static boolean[] visited;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		String[] str = br.readLine().split(" ");
+		n = Integer.parseInt(str[0]);
+		m = Integer.parseInt(str[1]);
+		
+		str = br.readLine().split(" ");
+		arr = new int[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		Arrays.sort(arr);
+		
+		result = new int[m];
+		visited = new boolean[n];
+		
+		perm(0);
+		System.out.println(sb);
+
+	}
+
+	private static void perm(int idx) {
+		if (idx == m) {
+			for (int n : result) {
+				sb.append(n + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i=0; i<n; i++) {
+			if (visited[i]) continue;
+			if (idx > 0 && result[idx-1] > arr[i]) continue;
+			
+			result[idx] = arr[i];
+			visited[i] = true;
+			perm(idx+1);
+			visited[i] = false;
+		}
+	}
+}
