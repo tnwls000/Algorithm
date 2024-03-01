5c9d1cef2294a273c15449f35131235a6c58ce3c
sujin <70851874+tnwls000@users.noreply.github.com>
2024-03-01 14:27:10 +0900

    [Silver II] Title: N과 M (9), Time: 436 ms, Memory: 52732 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	static int n, m;
+	static int[] result, arr;
+	static boolean[] visited;
+	static Set<String> set;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		String[] str = br.readLine().split(" ");
+		n = Integer.parseInt(str[0]);
+		m = Integer.parseInt(str[1]);
+		
+		str = br.readLine().split(" ");
+		arr = new int[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		Arrays.sort(arr);
+		
+		result = new int[m];
+		visited = new boolean[n];
+		set = new HashSet<String>();
+		
+		perm(0);
+		System.out.println(sb);
+	}
+
+	static void perm(int idx) {
+		if (idx == m) {
+			if (set.contains(Arrays.toString(result))) return;
+			
+			set.add(Arrays.toString(result));
+			for (int r : result) {
+				sb.append(r + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i=0; i<n; i++) {
+			if (visited[i]) continue;
+			
+			visited[i] = true;
+			result[idx] = arr[i];
+			perm(idx+1);
+			visited[i] = false;
+		}
+	}
+}
