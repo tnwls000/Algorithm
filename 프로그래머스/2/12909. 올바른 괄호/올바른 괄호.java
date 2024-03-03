5e2071e7cacd88074783e4c7b7cc105d33279dca
sujin <70851874+tnwls000@users.noreply.github.com>
2024-03-03 11:49:20 +0900

    [level 2] Title: 올바른 괄호, Time: 19.86 ms, Memory: 53.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        
+        if (!stack.isEmpty()) answer = false;
+        
+        return answer;
+    }
+}
