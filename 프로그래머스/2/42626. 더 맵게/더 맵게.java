fa0aa70aa9619d7017c1c8c58425139c588185fe
sujin <70851874+tnwls000@users.noreply.github.com>
2024-09-21 00:20:30 +0900

    [level 2] Title: 더 맵게, Time: 1801.61 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,22 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
+        int cnt = 0;
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i=0; i<scoville.length; i++) {
             pq.add(scoville[i]);
         }
         
-        int cnt = 0;
-        while (pq.size() >= 2) {
-            if (pq.peek() >= K) break;
+        while (!pq.isEmpty()) {
+            if (pq.size() == 1 & pq.peek() < K) return -1;
             
-            int num1 = pq.poll();
-            int num2 = pq.poll();
-            pq.add(num1 + (num2 * 2));
+            if (pq.peek() >= K) return cnt;
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
             cnt++;
+            pq.add(first + (second * 2));
         }
         
-        if (pq.peek() < K) return -1;
-        else return cnt;
+        return cnt;
     }
 }
