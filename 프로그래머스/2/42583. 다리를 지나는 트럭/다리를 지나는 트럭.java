f39e344f6852ea5f90349c090f96bb14c15324ff
sujin <70851874+tnwls000@users.noreply.github.com>
2024-12-28 21:40:12 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 83.55 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,46 +1,44 @@
 import java.util.*;
+
 class Solution {
+    static class Car {
+        int weight, time;
+        Car(int weight, int time) {
+            this.weight = weight;
+            this.time = time;
+        }
+    }
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 1;
+        int currW = truck_weights[0];
+        int truckIdx = 1;
         Queue<Car> q = new LinkedList<>();
-		q.add(new Car(truck_weights[0], 0)); // 첫 번째 트럭 넣어놓기
-		int totalWeight = truck_weights[0];
-		int idx = 1;
-		int time = 0;
-		while (true) {
-			time++;
-			// 트럭 이동
-			int length = q.size();
-			for (int i=0; i<length; i++) {
-				Car t = q.poll();
-				t.move++;
-				q.add(t);
-			}
-			
-			// 트럭 이동 길이 > 다리 길이 -> 큐에서 빼기
-			if (q.peek().move > bridge_length) {
-				totalWeight -= q.poll().weight;
-				if (q.isEmpty() && idx == truck_weights.length) break;
-			}
-			
-			if (idx < truck_weights.length) { // 다음 트럭 있을 때
-				// 현재 다리에 있는 트럭 무게 + 다음트럭 <= 하중
-				// 큐에 다음 트럭도 추가
-				if (totalWeight + truck_weights[idx] <= weight) {
-					totalWeight += truck_weights[idx];
-					q.add(new Car(truck_weights[idx++], 1));
-				}
-			}
-		}
+        
+        q.add(new Car(truck_weights[0], 1));
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i=0; i<size; i++) {
+                Car curr = q.poll();
+                if (curr.time == bridge_length) {
+                    currW -= curr.weight;
+                    continue;
+                }
+                curr.time++;
+                q.add(curr);
+            }
+            
+            time++;
+            if (q.size() == bridge_length) continue;
+            if (truckIdx == truck_weights.length) continue;
+            
+            int currTruckW = truck_weights[truckIdx];
+            if (currW + currTruckW > weight) continue;
+            q.add(new Car(currTruckW, 1));
+            currW += currTruckW;
+            truckIdx++;
+        }
+        
         return time;
     }
-}
-
-class Car {
-	int weight;
-	int move;
-	public Car(int weight, int move) {
-		super();
-		this.weight = weight;
-		this.move = move;
-	}
 }
