c78e0b2940dbf7e3e803001b9379ca27506cd1f4
sujin <70851874+tnwls000@users.noreply.github.com>
2025-01-02 23:37:14 +0900

    [level 2] Title: 올바른 괄호, Time: 19.84 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,16 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
         Stack<Character> stack = new Stack<>();
         for (int i=0; i<s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            } else {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    answer = false;
-                    break;
-                }
-            }
+            char c = s.charAt(i);
+            if (c == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') stack.pop();
+                else return false;
+            } else stack.push(c);
         }
         
-        if (!stack.isEmpty()) answer = false;
-        
-        return answer;
+        if (!stack.isEmpty()) return false;
+        return true;
     }
 }
