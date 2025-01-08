df5d4b22b45ed32dd219c761a47e5fb43210a983
sujin <70851874+tnwls000@users.noreply.github.com>
2025-01-08 22:41:20 +0900

    [level 2] Title: 구명보트, Time: 11.22 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int idx = 0;
+        
+        for (int i=people.length-1; i>=idx; i--) {
+            if (people[i] + people[idx] <= limit) idx++;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
